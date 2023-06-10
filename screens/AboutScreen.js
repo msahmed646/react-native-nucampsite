76e67d8f867ca26a9f452ba9c1e223a90e1829f0
msahmed646 <msahmed646@gmail.com>
2023-06-09 18:40:53 -0600

    usingRedux

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,6 +1,7 @@
-import { ScrollView } from 'react-native';
-import { Avatar, Card, ListItem, Text } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners'
+import { ScrollView, Text } from 'react-native';
+import { Avatar, Card, ListItem } from 'react-native-elements';
+import { useSelector } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
 
 const Mission = () => {
     return (
@@ -13,6 +14,7 @@ const Mission = () => {
     )
 }
 const AboutScreen = () => {
+    const partners = useSelector((state) => state.partners);
     return (
         <ScrollView>
             <Mission />
@@ -21,9 +23,9 @@ const AboutScreen = () => {
                 <Card.Title>Community Partners</Card.Title>
                 <Card.Divider />
 
-                {PARTNERS.map((partner) => (
+                {partners.partnersArray.map((partner) => (
                     <ListItem key={partner.id}>
-                        <Avatar rounded source={partner.image} />
+                        <Avatar rounded source={{ uri: baseUrl + partner.image}} />
                         <ListItem.Content>
                             <ListItem.Title>{partner.name}</ListItem.Title>
                             <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
---
+++ shared/baseUrl.js
@@ -0,0 +1,1 @@
+export const baseUrl = 'http://172.20.11.197:3001/';
