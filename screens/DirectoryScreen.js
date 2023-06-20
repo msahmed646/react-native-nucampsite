619b19f20f4e5663a43f9abcc64fe031a5df998a
msahmed646 <msahmed646@gmail.com>
2023-06-20 13:52:20 -0400

    Activity Indicator

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,10 +1,22 @@
-import { FlatList } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from '../components/LoadingComponent';
 
 const DirectoryScreen = ({ navigation }) => {
     const campsites = useSelector((state) => state.campsites);
+
+    if (campsites.isLoading) {
+        return <Loading />;
+    }
+    if (campsites.errMess) {
+        return (
+            <View>
+                <Text>{campsites.errMess}</Text>
+            </View>
+        );
+    }
 
     const renderDirectoryItem = ({ item: campsite }) => {
         return (
@@ -12,7 +24,7 @@ const DirectoryScreen = ({ navigation }) => {
                 title={campsite.name}
                 caption={campsite.description}
                 featured
-                onPress={() => 
+                onPress={() =>
                     navigation.navigate('CampsiteInfo', { campsite })
                 }
                 imageSrc={{ uri: baseUrl + campsite.image }}
