634636e78cc55557e99c2d189307825d075836bc
msahmed646 <msahmed646@gmail.com>
2023-06-23 12:45:48 -0400

    Local Notifications

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -6,11 +6,12 @@ import {
     StyleSheet,
     Switch,
     Button,
-    Alert,
+    Alert
 } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import * as Animatable from 'react-native-animatable'
+import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 const ReservationScreen = () => {
     const [campers, setCampers] = useState(1);
@@ -25,27 +26,36 @@ const ReservationScreen = () => {
     };
 
     const handleReservation = () => {
-        const reservationDetails = 
-            `Number of Campers: ${campers}\n
-            Hike-In? ${hikeIn}\n
-            Date: ${date.toLocaleDateString('en-US')}`;
-
+        const message = `Number of Campers: ${campers}
+                            \nHike-In? ${hikeIn}
+                            \nDate: ${date.toLocaleDateString('en-US')}`;
         Alert.alert(
             'Begin Search?',
-            reservationDetails,
+            message,
             [
                 {
                     text: 'Cancel',
-                    style: 'cancel',
-                    onPress: resetForm()
+                    onPress: () => {
+                        console.log('Reservation Search Canceled');
+                        resetForm();
+                    },
+                    style: 'cancel'
                 },
                 {
                     text: 'OK',
-                    onPress: () => console.log('OK Pressed')
+                    onPress: () => {
+                        presentLocalNotification(
+                            date.toLocaleDateString('en-US')
+                        );
+                        resetForm();
+                    }
                 }
             ],
             { cancelable: false }
-        )
+        );
+        console.log('campers:', campers);
+        console.log('hikeIn:', hikeIn);
+        console.log('date:', date);
     };
 
     const resetForm = () => {
@@ -55,9 +65,37 @@ const ReservationScreen = () => {
         setShowCalendar(false);
     };
 
+    const presentLocalNotification = async (reservationDate) => {
+        const sendNotification = () => {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true,
+                    shouldPlaySound: true,
+                    shouldSetBadge: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${reservationDate} requested`
+                },
+                trigger: null
+            });
+        };
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    };
+
     return (
         <ScrollView>
-            <Animatable.View animation="zoomIn" duration={2000} delay={1000}>
+            <Animatable.View animation='zoomIn' duration={2000} delay={1000}>
                 <View style={styles.formRow}>
                     <Text style={styles.formLabel}>Number of Campers:</Text>
                     <Picker
@@ -91,7 +129,6 @@ const ReservationScreen = () => {
                         accessibilityLabel='Tap me to select a reservation date'
                     />
                 </View>
-            
                 {showCalendar && (
                     <DateTimePicker
                         style={styles.formItem}
@@ -127,9 +164,8 @@ const styles = StyleSheet.create({
         flex: 2
     },
     formItem: {
-        flex: 1,
-
-    },
+        flex: 1
+    }
 });
 
 export default ReservationScreen;
